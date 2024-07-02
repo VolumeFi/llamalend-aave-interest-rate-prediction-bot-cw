2c4a6102e9b92cc05aaf2cf3dc45e0683c90f999
fullstack412 <jaclindev99@gmail.com>
2024-07-02 22:45:25 +0800

    update contract name

--- src/bin/schema.rs
+++ src/bin/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use llamalend_crv_prediction_bot_cw::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use llamalend_aave_interest_rate_prediction_bot_cw::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
